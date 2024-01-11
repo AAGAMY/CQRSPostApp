54b19a53e106be8e6ae924efdd43a6634a4692b6
AGAMY <Ahmed.ElSayedMustafa@microtec.com.eg>
2024-01-11 14:53:41 +0200

    Update

---
+++ Post.Application/Contracts/IAsyncRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Application.Contracts;
+public interface IAsyncRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(Guid id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+
+}
---
+++ Post.Application/Contracts/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Post.Application.Contracts;
+public interface IPostRepository : IAsyncRepository<Post.Domain.Post>
+{
+    Task<IReadOnlyList<Post.Domain.Post>> GetAllPostsAsync(bool includeCategory);
+    Task<Post.Domain.Post> GetPostByIdAsync(Guid id, bool includeCategory);
+}
