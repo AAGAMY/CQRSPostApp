e3b10bb487b137ab7d1a5dbe0c4922987c297b5d
AGAMY <Ahmed.ElSayedMustafa@microtec.com.eg>
2024-01-11 13:25:14 +0200

    Add project files.

---
+++ Post.Domain/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Domain;
+public class Post
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public Category? Category { get; set; }
+    public Guid CategoryId { get; set; }
+
+}
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public ICollection<Post>? Blogs { get; set; }
+
+}
