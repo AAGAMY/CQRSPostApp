b107d0cb48f757bd342be66a7658f244c1f81fcc
AGAMY <Ahmed.ElSayedMustafa@microtec.com.eg>
2024-01-14 14:52:41 +0200

    Update

--- Post.Application/Profiles/MapProfile.cs
+++ Post.Application/Profiles/MapProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Post.Application.Features.Post.Commands.CreatePost;
+using Post.Application.Features.Post.Commands.DeletePost;
 using Post.Application.Features.Post.Commands.UpdatePost;
 using Post.Application.Features.Post.Queries.GetPostDetails;
 using Post.Application.Features.Post.Queries.GetPostList;
+using Posts.Domain;
 
 namespace Post.Application.Profiles;
 public class MapProfile : Profile
@@ -13,5 +15,7 @@ public class MapProfile : Profile
         CreateMap<Posts.Domain.Post, GetPostDetailViewModel>().ReverseMap();
         CreateMap<Posts.Domain.Post, CreatePostCommand>().ReverseMap();
         CreateMap<Posts.Domain.Post, UpdatePostCommand>().ReverseMap();
+        CreateMap<Posts.Domain.Post, DeletePostCommand>().ReverseMap();
+        CreateMap<Category, CategoryDto>().ReverseMap();
     }
 }
