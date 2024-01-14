0a17ebb27650a2c664f518b9df12c8812f9f3098
AGAMY <Ahmed.ElSayedMustafa@microtec.com.eg>
2024-01-14 11:19:09 +0200

    Create DbContext

---
+++ Post.Persistence/PostDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Posts.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Persistence;
+public class PostDbContext : DbContext
+{
+    public PostDbContext(DbContextOptions<PostDbContext> options) : base(options) { }
+
+    public PostDbContext()
+    {
+        
+    }
+
+    public DbSet<Posts.Domain.Post> Posts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var categoryGuid = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}");
+        var postGuid = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}");
+        modelBuilder.Entity<Category>().HasData(new Category
+        {
+
+            Id = categoryGuid,
+            Name = "Technology"
+
+        });
+
+        modelBuilder.Entity<Posts.Domain.Post>().HasData(new Posts.Domain.Post
+        {
+            Id = postGuid,
+            Title = "Introduction to CQRS and Mediator Patterns",
+            Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            ImageUrl = "https://api.khalidessaadani.com/uploads/articles_bg. jpg",
+            CategoryId = categoryGuid
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Post.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Post.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Persistence.Repositories;
+public class BaseRepository<T> : IAsyncRepository<T> where T : class
+{
+    private readonly PostDbContext _context;
+
+    public BaseRepository(PostDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<T> AddAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public virtual async Task<T> GetByIdAsync(Guid id)
+    {
+        return await _context.Set<T>().FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        return await _context.Set<T>().ToListAsync();
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+}
