de80e147b7f364ccf4d7b32e7c6c73e02aed9e28
AGAMY <Ahmed.ElSayedMustafa@microtec.com.eg>
2024-01-14 13:28:41 +0200

    Update

---
+++ Post.Application/ApplicationContainer.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Application;
+public static class ApplicationContainer
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
---
+++ Post.Persistence/PersistenceContainer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Post.Application.Contracts;
+using Post.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Persistence;
+public static class PersistenceContainer
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<PostDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("PostConnectionString")));
+
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+        services.AddScoped(typeof(IPostRepository), typeof(PostRepository));
+
+        return services;
+
+
+
+    }
+}
